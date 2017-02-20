e4b1fc06dfd0a6901f08c21a1d70f0ba36ddc90d
mirinth <mirinth@gmail.com>
2017-02-20 09:05:50 -0700

    Refactored deleter

--- content/deleter.js
+++ content/deleter.js
@@ -11,7 +11,7 @@ start();
 
 function start() {
 	console.log('delete: starting');
-	step = waitForLoad;
+	step = openMenu;
 	doStep(0);
 }
 
@@ -35,48 +35,34 @@ function doStep(tick) {
 	window.setTimeout(doStep, tickInterval, tick + 1);
 }
 
-function waitForLoad() {
-	if (isDeleted()) {
+function openMenu() {
+	let deleteMessage = getElement('div', isDeleteMessage);
+	if (deleteMessage) {
 		step = done;
 		return;
 	}
 
-	let arrow = getArrow();
+	let arrow = getElement('a', isArrow);
 	if (arrow) {
-		step = openDropdown;
+		arrow.click();
+		step = clickDeletePostOption;
 	}
 }
 
-function openDropdown() {
-	let arrow = getArrow();
-	arrow.click();
-	step = waitForDropdownOpened;
-}
-
-function waitForDropdownOpened() {
-	let delPostButton = getDeletePostButton();
-	if (delPostButton) {
-		step = clickDeletePost;
+function clickDeletePostOption() {
+	let deletePostOption = getElement('div', isDeletePostOption);
+	if (deletePostOption) {
+		deletePostOption.click();
+		step = clickDeleteButton;
 	}
 }
 
-function clickDeletePost() {
-	let deletePostButton = getDeletePostButton();
-	deletePostButton.click();
-	step = waitForDeleteAvailable;
-}
-
-function waitForDeleteAvailable() {
-	let deleteButton = getDeleteButton();
+function clickDeleteButton() {
+	let deleteButton = getElement('button', isDeleteButton);
 	if (deleteButton) {
-		step = clickDelete
+		deleteButton.click();
+		step = done;
 	}
-}
-
-function clickDelete() {
-	let deleteButton = getDeleteButton();
-	deleteButton.click();
-	step = done;
 }
 
 function done() {
@@ -93,57 +79,31 @@ function done() {
 	});
 }
 
-function isDeleted() {
+function getElement(tagName, predicate) {
+	let elems = document.getElementsByTagName(tagName);
+	for (let elem of elems) {
+		if (predicate(elem)) {
+			return elem;
+		}
+	}
+
+	return null;
+}
+
+function isDeleteMessage(elem) {
 	let content = 'This post has been removed or could not be loaded.';
-	let elems = document.getElementsByTagName('div');
-
-	for (let elem of elems) {
-		if (elem.innerHTML === content) {
-			return true;
-		}
-	}
-
-	return false;
+	return (elem.innerHTML === content);
 }
 
-function getArrow() {
-	let elems = document.getElementsByTagName('a');
-	for (let elem of elems) {
-		if (elem.getAttribute('aria-label') === 'Story options') {
-			return elem;
-		}
-	}
-
-	return null;
+function isArrow(elem) {
+	return (elem.getAttribute('aria-label') === 'Story options');
 }
 
-function getDeletePostButton() {
-	let elems = document.getElementsByTagName('div');
-	for (let elem of elems) {
-		if (isDeletePostButton(elem)) {
-			return elem;
-		}
-	}
-
-	return null;
-}
-
-function isDeletePostButton(elem) {
+function isDeletePostOption(elem) {
 	return (
 		elem.childNodes.length === 3 &&
 		elem.childNodes[2].nodeValue === 'Delete post')
 	);
-}
-
-function getDeleteButton() {
-	let elems = document.getElementsByTagName('button');
-	for (let elem of elems) {
-		if (isDeleteButton(elem)) {
-			return elem;
-		}
-	}
-
-	return null;
 }
 
 function isDeleteButton(elem) {
