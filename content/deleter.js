30c100f5bc7820e52a40ecb9a95392763f076b1b
mirinth <mirinth@gmail.com>
2017-02-19 19:37:05 -0700

    Fixed brace style

--- content/deleter.js
+++ content/deleter.js
@@ -9,28 +9,23 @@ let monitorPulse = undefined;
 console.log("deleter injected");
 start();
 
-function start()
-{
+function start() {
 	console.log('delete: starting');
 	step = waitForLoad;
 	doStep(0);
 }
 
-function doStep(tick)
-{
-	if (tick % 25 === 0)
-	{
+function doStep(tick) {
+	if (tick % 25 === 0) {
 		console.log('delete: waited ' + tick + '/' + timeout + ' ticks before timeout');
 	}
-	if (tick > timeout && prevStep === step)
-	{
+	if (tick > timeout && prevStep === step) {
 		console.log('delete: timed out on page: ' + window.location.href);
 		step = done;
 		step();
 		return;
 	}
-	if (prevStep !== step)
-	{
+	if (prevStep !== step) {
 		prevStep = step;
 		tick = 0;
 	}
@@ -39,84 +34,69 @@ function doStep(tick)
 	window.setTimeout(doStep, tickInterval, tick + 1);
 }
 
-function waitForLoad()
-{
+function waitForLoad() {
 	let arrow = getArrow();
 
-	if (isDeleted())
-	{
+	if (isDeleted()) {
 		console.log('delete: post already deleted');
 		step = done;
 	}
-	else if (arrow === null)
-	{
+	else if (arrow === null) {
 		console.log('delete: page not loaded yet');
 	}
-	else
-	{
+	else {
 		console.log('delete: page loaded');
 		step = openDropdown;
 	}
 }
 
-function openDropdown()
-{
+function openDropdown() {
 	console.log('delete: opening drop down');
 	let arrow = getArrow();
 	arrow.click();
 	step = waitForDropdownOpened;
 }
 
-function waitForDropdownOpened()
-{
+function waitForDropdownOpened() {
 	let delPostButton = getDeletePostButton();
 
-	if (delPostButton === null)
-	{
+	if (delPostButton === null) {
 		console.log('delete: dropdown not open yet');
 	} 
-	else
-	{
+	else {
 		console.log('delete: dropdown opened');
 		step = clickDeletePost;
 	}
 }
 
-function clickDeletePost()
-{
+function clickDeletePost() {
 	console.log('delete: clicking Delete Post button');
 	let deletePostButton = getDeletePostButton();
 	deletePostButton.click();
 	step = waitForDeleteAvailable;
 }
 
-function waitForDeleteAvailable()
-{
+function waitForDeleteAvailable() {
 	let deleteButton = getDeleteButton();
 
-	if (deleteButton === null)
-	{
+	if (deleteButton === null) {
 		console.log('delete: dialog not open yet');
-	} 
-	else
-	{
+	}
+	else {
 		console.log('delete: dialog opened');
 		step = clickDelete
 	}
 }
 
-function clickDelete()
-{
+function clickDelete() {
 	console.log('delete: deleting the post');
 	let deleteButton = getDeleteButton();
 	deleteButton.click();
 	step = done;
 }
 
-function done()
-{
-	if (!isFinished)
-	{
+function done() {
+	if (!isFinished) {
 		isFinished = true;
 		console.log('delete: done!');
 		browser.runtime.sendMessage({
@@ -126,15 +106,12 @@ function done()
 	}
 }
 
-function isDeleted()
-{
+function isDeleted() {
 	let elems = document.getElementsByTagName('div');
 
-	for (let index = 0; index < elems.length; index++)
-	{
+	for (let index = 0; index < elems.length; index++) {
 		if (elems[index].innerHTML === 
-			'This post has been removed or could not be loaded.')
-		{
+			'This post has been removed or could not be loaded.') {
 			return true;
 		}
 	}
@@ -142,8 +119,7 @@ function isDeleted()
 	return false;
 }
 
-function getArrow()
-{
+function getArrow() {
 	let elems = document.getElementsByTagName('a');
 	for (let elem of elems) {
 		if (elem.getAttribute('aria-label') === 'Story options') {
@@ -154,15 +130,12 @@ function getArrow()
 	return null;
 }
 
-function getDeletePostButton()
-{
+function getDeletePostButton() {
 	let elems = document.getElementsByTagName('div');
 
-	for (let index = 0; index < elems.length; index++)
-	{
+	for (let index = 0; index < elems.length; index++) {
 		if (elems[index].childNodes.length === 3 &&
-			elems[index].childNodes[2].nodeValue === 'Delete Post')
-		{
+			elems[index].childNodes[2].nodeValue === 'Delete Post') {
 			return elems[index];
 		}
 	}
@@ -170,18 +143,15 @@ function getDeletePostButton()
 	return null;
 }
 
-function getDeleteButton()
-{
+function getDeleteButton() {
 	let deleteMessage = "Delete";
 	let noSoldMessage = "Haven't Sold";
 
 	let elems = document.getElementsByClassName('uiOverlayButton');
 
-	for (let index = 0; index < elems.length; index++)
-	{
+	for (let index = 0; index < elems.length; index++) {
 		if (elems[index].innerHTML === deleteMessage ||
-			elems[index].innerHTML === noSoldMessage)
-		{
+			elems[index].innerHTML === noSoldMessage) {
 			return elems[index];
 		}
 	}
