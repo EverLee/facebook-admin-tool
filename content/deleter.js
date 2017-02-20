89e32ab7b68e16be82e9ba17e3c915a6883de2e7
mirinth <mirinth@gmail.com>
2017-02-20 09:14:35 -0700

    Fixed syntax error

--- content/deleter.js
+++ content/deleter.js
@@ -102,7 +102,7 @@ function isArrow(elem) {
 function isDeletePostOption(elem) {
 	return (
 		elem.childNodes.length === 3 &&
-		elem.childNodes[2].nodeValue === 'Delete post')
+		elem.childNodes[2].nodeValue === 'Delete post'
 	);
 }
 
