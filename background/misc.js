7a4bbe726b91b132a1e03fc65cc8847b4c4495f2
mirinth <mirinth@gmail.com>
2017-02-20 18:07:53 -0700

    Removed unused update handler

--- background/misc.js
+++ background/misc.js
@@ -1,6 +1,5 @@
 let permalinks = [];
 browser.browserAction.onClicked.addListener(handleButton);
-browser.runtime.onInstalled.addListener(onUpdated);
 
 function handleButton() {
 	if (permalinks.length > 0) {
@@ -10,13 +9,4 @@ function handleButton() {
 	else {
 		extractPosts();
 	}
-}
-
-function onUpdated(details) {
-	if (details.reason !== "update") {
-		return;
-	}
-
-	let creating = browser.tabs.create({ url: "/pages/changelog.html" });
-	creating.then(function(){}, function(reason){ console.log(reason); });
 }
